0719910c2b1b0dc05c0311bdc56cf9455fd37947
kbomeisl <kaybomeisl2@gmail.com>
2022-11-09 12:29:37 -0500

    Layout of Chat Screen completed to specifications

--- app/src/main/java/com/datechnologies/androidtest/chat/ChatScreen.kt
+++ app/src/main/java/com/datechnologies/androidtest/chat/ChatScreen.kt
@@ -1,18 +1,25 @@
 package com.datechnologies.androidtest.chat
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -32,8 +39,9 @@ fun ChatScreen(
         Header(navController, "Chat", true)
         LazyColumn {
             items(chatViewModel.message) { chat ->
+                Spacer(modifier = Modifier.height(16.dp))
                 ChatCard(chatLogMessageModel = chat) 
-                Spacer(modifier = Modifier.height(10.dp))
+
             }
             }
         }
@@ -65,9 +73,26 @@ fun ChatScreen(
 
                 }
                 Column() {
-                    Text(text = chatLogMessageModel.name.toString())
-                    Text(text = chatLogMessageModel.message.toString())
+                    Text(
+                        text = chatLogMessageModel.name.toString(),
+                        fontSize = 13.sp,
+                        color = Color(0xFF1B1E1F),
+                        fontWeight = FontWeight.Medium
+                    )
+                Surface(modifier = Modifier
+                    .padding(8.dp)
+                    .border(1.dp, Color(0xFFEFEFEF),shape = RoundedCornerShape(8.dp))
+
+                ) {
+                        Text(modifier = Modifier
+                            .padding(8.dp),
+                            text = chatLogMessageModel.message.toString(),
+                            fontSize = 15.sp,
+                            color = Color(0xFF1B1E1F)
+                            )
+                    }
                 }
+
 
             }
         }
